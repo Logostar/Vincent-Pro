30bb955ba4cd212ba58b147c5d683d76ac0e174b
Logostar <74246251+Logostar@users.noreply.github.com>
2020-12-17 06:16:29 +0000

    Update Video.js

--- js/Video.js
+++ js/Video.js
@@ -1,41 +1,71 @@
-/**
-* Created by Lete on 2020/6/16.
-*/
-//判断移动端设备
-browserRedirect();
+var sUserAgent = navigator.userAgent.toLowerCase();
+var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+var bIsAndroid = sUserAgent.match(/android/i) == "android";
+var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM)) {
+    // 判断 header里是否存在full_page
+    if($("#page-header").hasClass("full_page")){
+      // 插入<video>
+      var html = "<video id='video' muted loop autoplay style='height: 100%;width: 100%;object-fit: cover;'></video>";
+      document.getElementsByClassName("full_page")[0].innerHTML += html;
+      // 随机数
+      var randomNum = Math.floor((Math.random() * 15)+1);
+      // 获取 <video>
+      var video = document.getElementById('video');
+      // 插入视频(m3u8)
+      var videoSrc = 'https://cdn.jsdelivr.net/gh/lete114/CDN2/video/'+randomNum+'.m3u8';
+      if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(videoSrc);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function() {
+          video.play();
+        });
+      }
+    }
+}else{
+  // 判断 header里是否存在full_page
+  if($("#page-header").hasClass("full_page")){
+    // 插入<video>
+    var html = "<video id='video' muted loop autoplay style='height: 100%;width: 100%;object-fit: cover;'></video>";
+    document.getElementsByClassName("full_page")[0].innerHTML += html;
+    // 随机数
+    var randomNum = Math.floor((Math.random() * 6)+1);
+    // 获取 <video>
+    var video = document.getElementById('video');
+    // 插入视频(m3u8)
+    var videoSrc = 'https://cdn.jsdelivr.net/gh/lete114/CDN2/video/mobile/'+randomNum+'.m3u8';
+    if (Hls.isSupported()) {
+      var hls = new Hls();
+      hls.loadSource(videoSrc);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, function() {
+        video.play();
+      });
+    }
+  }
+}
+if($("#page-header").hasClass("full_page")){
+  var sub = new Array("人生只有一次，大胆的生活！！", "Life only once, live boldly!!");
 
-function browserRedirect() {
-   var sUserAgent = navigator.userAgent.toLowerCase();
-   var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-   var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-   var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-   var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-   var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-   var bIsAndroid = sUserAgent.match(/android/i) == "android";
-   var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-   var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
-   if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM)) {
-       var index_img = "<video loop autoplay style='height: 100%;width: 100%;object-fit: cover;'><source src='https://cdn.jsdelivr.net/gh/Logostar/Vincent-Pro/source/video/home.mp4'></source></video>";
-       /*添加video*/
-       document.getElementById("nav").innerHTML += index_img;
+  var randomSub = Math.floor((Math.random() * sub.length));
 
-       /*打字效果*/
-       var subtitle = document.getElementById('subtitle')
-       var i = 0;
-
-       function typing() {
-           var timer = 0;
-           var str = '一人一人には自分の時間軸があります。';
-           if (i <= str.length) {
-               subtitle.innerHTML = str.slice(0, i++)
-               timer = setTimeout(typing, 300)
-
-           } else {
-               subtitle.innerHTML = str
-               clearTimeout(timer)
-           }
-       }
-
-       typing()
-   }
+  var subtitle = document.getElementById('subtitle')
+  var i = 0; 
+  function typing() {
+    var timer = 0; var str = sub[randomSub];
+    if (i <= str.length) {
+        subtitle.innerHTML = str.slice(0, i++)
+        timer = setTimeout(typing, 200)
+    }else{
+      subtitle.innerHTML = str
+      clearTimeout(timer)
+    }
+  }
+  typing()
 }
